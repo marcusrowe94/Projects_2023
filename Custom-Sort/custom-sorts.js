c593ffb237575c4759bb2ea7ddd03e40e9a98b79
marcus <marcusrowe973636@outlook.com>
2023-05-01 04:53:45 -0400

    Final Solve For Custom Sort

--- Custom-Sort/custom-sorts.js
+++ Custom-Sort/custom-sorts.js
@@ -37,28 +37,50 @@ function oddEvenSort(arr) {
 }
 
 function validAnagrams(s, t) {
-  let word1 = s.split("")
-  let word2 = t.split("")
-  while(word1.length === word2.length){
- let sorted1 = word1.sort().join(",")
-  let sorted2 = word2.sort().join(",")
-  console.log(sorted1)
-  console.log(sorted2)
-    if(sorted1 === sorted2){
-      return true
+  let word1 = s.split("");
+  let word2 = t.split("");
+  while (word1.length === word2.length) {
+    let sorted1 = word1.sort().join(",");
+    let sorted2 = word2.sort().join(",");
+    console.log(sorted1);
+    console.log(sorted2);
+    if (sorted1 === sorted2) {
+      return true;
     }
-    return false
+    return false;
   }
-
 }
 
 function reverseBaseSort(arr) {
-  // Your code here
+  return arr.sort((a, b) => {
+    const aLog = parseInt(Math.log10(a));
+    const bLog = parseInt(Math.log10(b));
+    if (aLog === bLog) return a - b;
+    else {
+      return bLog - aLog;
+    }
+  });
 }
 
 function frequencySort(arr) {
-  // Your code here
+  const count = {};
+  for (const num of arr) {
+    if (count[num] !== undefined) {
+      count[num]++;
+    } else {
+      count[num] = 1;
+    }
+  }
+  arr.sort((a, b) => {
+    if (count[a] === count[b]) {
+      return b - a;
+    } else {
+      return count[a] - count[b];
+    }
+  });
+  return arr;
 }
+
 
 module.exports = [
   oddEvenSort,
