1f1729487da07fa04b148211b7245b85cd0a2971
marcus <marcusrowe94@gmail.com>
2023-05-28 00:24:02 -0400

    Unit-Test-Update

--- Unit_testing_Practice/funcs.js
+++ Unit_testing_Practice/funcs.js
@@ -1,14 +1,22 @@
 function isFive(num) {
-  // Your code here
+  if (num === 5) return true;
+  else return false 
 }
 
 function isOdd(number) {
-  // Your code here
+
+if (typeof number !== "number") throw new Error();
+if (number % 2 === 0) return false;
+else return true
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
-}
+  let arr = []
+  for( let i = min; i <= max; i +=step){
 
+    arr.push(i)
+  }
+  return arr
+}
 
 module.exports = { isFive, isOdd, myRange };
