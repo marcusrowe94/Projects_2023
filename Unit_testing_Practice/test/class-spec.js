2f0d842c94c624fee996bd105eaed9a7d146169a
marcus <marcusrowe94@gmail.com>
2023-05-28 01:45:47 -0400

    TDD-Added-To-Completion

--- Unit_testing_Practice/test/class-spec.js
+++ Unit_testing_Practice/test/class-spec.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 
 const { Word } = require("../class");
 
+
   const word = new Word()
 
   describe("Word", function () {
@@ -19,19 +20,28 @@ const { Word } = require("../class");
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const word = new Word('hello');
+      expect(word.removeVowels()).equal('hll');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      const word1 = new Word("Butterfly")
+      expect(word1.removeConsonants()).equal("ue");
     });
   });
 
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const word1 = new Word('apple');
+    expect(word1.pigLatin()).equal('appleyay');
+
+    const word2 = new Word('banana');
+    expect(word2.pigLatin()).equal('ananabay');
+
+    const word3 = new Word('hello');
+    expect(word3.pigLatin()).equal('ellohay')
     });
   });
 });
