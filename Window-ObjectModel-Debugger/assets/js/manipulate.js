febf569555624ed6dabb3d4f859bd40b4b8e2f36
marcus <marcusrowe94@gmail.com>
2023-07-11 18:58:29 -0400

    green

---
+++ Window-ObjectModel-Debugger/assets/js/manipulate.js
@@ -0,0 +1,19 @@
+export function changeTitle() {
+  // Change the title of the page to "(Your name)'s Portfolio"
+  const title = document.getElementById("h1");
+  console.log(title);
+  return (title.innerText = "Marcus Rowe's Portfolio");
+  // Your code here
+  debugger
+}
+
+export function changeHeader() {
+  // Change the name in the h1 of the page to your name
+  // Your code here
+}
+
+export function changeAboutMe() {
+  /* Update the first paragraph in the About Me section with a small
+     passage about yourself */
+  // Your code here
+}
