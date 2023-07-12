adcc2d6de87c421b6bcb51eb0e2eae6b0220d592
marcus <marcusrowe94@gmail.com>
2023-07-11 22:01:00 -0400

    final solve 2

--- Window-ObjectModel-Debugger/assets/js/index.js
+++ Window-ObjectModel-Debugger/assets/js/index.js
@@ -1,17 +1,14 @@
-import * as Manipulate from "./manipulate.js";
-import * as findingElements from "./search.js"
-window.addEventListener("DOMContentLoaded", (event) => {
-  Manipulate.changeTitle();
-  Manipulate.changeHeader();
-  Manipulate.changeAboutMe();
-  findingElements.findElementById()
-  findingElements.findFirstElementOfTag()
-  findingElements.findFirstElementOfClass()
-  findingElements.findElementsOfTag()
-  findingElements.findElementsOfClass()
-  console.log(findElementById("header")); // <div id="header">
-  console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
-  console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
-  console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
-  console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
-});
+import { changeAboutMe, changeTitle, changeHeader } from "./manipulate";
+import { findElementById, findFirstElementOfTag, findFirstElementOfClass, findElementsOfClass, findElementsOfTag} from "./search";
+
+window.addEventListener("DOMContentLoaded",event =>{
+
+    changeTitle();
+    changeHeader();
+    changeAboutMe();
+    console.log(findElementById("header")); // <div id="header">
+    console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
+    console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
+    console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
+    console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+})
